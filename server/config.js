d3428c53830e9a0dbc57d219153ed433040f6545
“cx330â? <1175963382@qq.com>
2023-04-15 23:14:40 +0800

    修改了一下

--- server/config.js
+++ server/config.js
@@ -15,64 +15,36 @@ const prizes = [
   },
   {
     type: 1,
-    count: 2,
-    text: "特等奖",
-    title: "神秘大礼",
+    count: 1,
+    text: "一等奖",
+    title: "星球灯",
     img: "../img/secrit.jpg"
   },
   {
     type: 2,
-    count: 5,
-    text: "一等奖",
-    title: "Mac Pro",
+    count: 2,
+    text: "二等奖",
+    title: "海豚抱枕",
     img: "../img/mbp.jpg"
   },
   {
     type: 3,
-    count: 6,
-    text: "二等奖",
-    title: "华为 Mate30",
-    img: "../img/huawei.png"
-  },
-  {
-    type: 4,
-    count: 7,
+    count: 3,
     text: "三等奖",
-    title: "Ipad Mini5",
-    img: "../img/ipad.jpg"
-  },
-  {
-    type: 5,
-    count: 8,
-    text: "四等奖",
-    title: "大疆无人机",
-    img: "../img/spark.jpg"
-  },
-  {
-    type: 6,
-    count: 8,
-    text: "五等奖",
-    title: "Kindle",
-    img: "../img/kindle.jpg"
-  },
-  {
-    type: 7,
-    count: 11,
-    text: "六等奖",
-    title: "漫步者蓝牙耳机",
-    img: "../img/edifier.jpg"
+    title: "U盘",
+    img: "../img/huawei.png"
   }
 ];
 
 /**
  * 一次抽取的奖品个数与prizes对应
  */
-const EACH_COUNT = [1, 1, 5, 6, 7, 8, 9, 10];
+const EACH_COUNT = [1, 1, 2, 3];
 
 /**
  * 卡片公司名称标识
  */
-const COMPANY = "MoShang";
+const COMPANY = "Ace魔术社";
 
 module.exports = {
   prizes,
